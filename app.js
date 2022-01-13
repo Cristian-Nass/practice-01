565a4593667f808a4015d26c9e5a986f627e7432
Cristian <cristian.tirdad@gmail.com>
2022-01-13 18:34:54 +0100

    add button and function

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finish the course and learn Vue!',
-      vueLink: 'https://vuejs.org/'
+      courseGoalA: 'Finish the course and learn Vue!',
+      courseGoalB: 'Master of Vue!',
+      vueLink: 'https://vuejs.org/',
+      imgLink: 'https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg',
+      counter: 0
     };
   },
   methods: {
     outputGoal() {
       const randomNumber = Math.random();
-      return randomNumber > 0.5 ? 'LearnVue' : 'Master Vue'
+      return randomNumber > 0.5 ? this.courseGoalA : this.courseGoalB
     }
   }
 });
