ab160c4b8c87c2c9f9de80e6c506fff806369222
Cristian <cristian.tirdad@gmail.com>
2022-01-17 16:46:11 +0100

    random

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ const app = Vue.createApp({
       courseGoalB: 'Master of Vue!',
       vueLink: 'https://vuejs.org/',
       imgLink: 'https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg',
-      counter: 0
+      counter: 0,
+      name: ''
     };
   },
   methods: {
     outputGoal() {
       const randomNumber = Math.random();
       return randomNumber > 0.5 ? this.courseGoalA : this.courseGoalB
+    },
+    increment() {
+      this.counter++;
+    },
+    setName(event) {
+      this.name = event.target.value;
     }
   }
 });
